26064b9f54b3e42a4fd548543378872aeda7a4b2
tsuyoshi <nakasone@std.it-college.ac.jp>
2014-09-27 13:02:45 +0900

    プログラム構造変更

--- app/src/main/java/jp/ac/it_college/nakasone/android/sequentialnumbers/TitleActivity.java
+++ app/src/main/java/jp/ac/it_college/nakasone/android/sequentialnumbers/TitleActivity.java
@@ -16,29 +16,11 @@ public class TitleActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new TouchStartFragment())
-                    .commit();
-        }
     }
 
-    private void startExamActivity() {
+    public void startExamActivity(View v) {
         Intent intent = new Intent(getApplicationContext(), ExamActivity.class);
         startActivity(intent);
     }
 
-    public static class TouchStartFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_touch, container, false);
-            view.findViewById(R.id.text_touch).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ((TitleActivity)getActivity()).startExamActivity();
-                }
-            });
-            return view;
-        }
-    }
 }
